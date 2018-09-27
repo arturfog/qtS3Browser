ca7bc9510ec18ef3256a28b0d03768096d5bab68
Artur Fogiel <arturprojekty1988@gmail.com>
2018-09-27 17:48:41 +0200

    Added debian directory for qt

---
+++ deb_qt/checkout.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# download
+#wget http://download.qt.io/official_releases/qt/5.10/5.10.1/single/qt-everywhere-src-5.10.1.tar.xz
+# unpack
+tar -xf  qt-everywhere-src-5.10.1.tar.xz
+
+mv qt-everywhere-src-5.10.1 qt-everywhere-5.10.1
--- deb_qt/create_deb_qt.sh
+++ deb_qt/create_deb_qt.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 #
-# This script creates QT 5.10 debian package
-
+# This script creates QT 5.10.1 debian package
+cp -vfr debian qt-everywhere-5.10.1
+cd qt-everywhere-5.10.1
 # Build package
 dpkg-buildpackage -rfakeroot -I.git
