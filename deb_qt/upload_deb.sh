8b53d8563f1858d96148e034bebc2814c344e817
Artur Fogiel <arturprojekty1988@gmail.com>
2018-09-29 20:35:59 +0000

    Attempt to switch snap and travis to custom build .deb pacakges

---
+++ deb_qt/upload_deb.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+if [ -z "$1" ]; then
+  echo 'Please execute with gpg password: ./upload_deb.sh [gpg password]'
+  exit
+fi
+
+if [ -z "$API_KEY" ]; then
+  echo 'Please export your bintray api key as env variable API_KEY: export API_KEY="0000000000000000"'
+  exit
+fi
+
+curl -v -T 'qt-everywhere_5.9.6_amd64.deb' -H 'X-Bintray-Debian-Distribution: xenial' -H 'X-Bintray-Debian-Component: main' -H 'X-Bintray-Debian-Architecture: amd64' -H "X-GPG-PASSPHRASE: $1" -uarturfog:${API_KEY} https://api.bintray.com/content/arturfog/oss-arturfog/qt-everywhere/5.9.6/qt-everywhere_5.9.6_amd64.deb
