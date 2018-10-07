59c1f2c8b053a9a663682d8907a27eb443e97511
Artur <arturprojekty1988@gmail.com>
2018-10-07 20:25:10 +0200

    Develop (#4)
    * Configuring auto deb deploy from Travis

--- deb_qt/upload_deb.sh
+++ deb_qt/upload_deb.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
-if [ -z "$1" ]; then
-  echo 'Please execute with gpg password: ./upload_deb.sh [gpg password]'
-  exit
-fi
-
 if [ -z "$API_KEY" ]; then
   echo 'Please export your bintray api key as env variable API_KEY: export API_KEY="0000000000000000"'
   exit
 fi
 
-curl -v -T 'qt-everywhere_5.10.0_amd64.deb' -H 'X-Bintray-Debian-Distribution: xenial' -H 'X-Bintray-Debian-Component: main' -H 'X-Bintray-Debian-Architecture: amd64' -H "X-GPG-PASSPHRASE: $1" -uarturfog:${API_KEY} https://api.bintray.com/content/arturfog/oss-arturfog/qt-everywhere/5.10.0/qt-everywhere_5.10.0_amd64.deb
+curl -v -T 'qt-everywhere_5.10.0_amd64.deb' -H 'X-Bintray-Debian-Distribution: stretch' -H 'X-Bintray-Debian-Component: main' -H 'X-Bintray-Debian-Architecture: amd64' -uarturfog:${API_KEY} https://api.bintray.com/content/arturfog/oss-arturfog/qt-everywhere/5.10.0/qt-everywhere_5.10.0_amd64.deb
