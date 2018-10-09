3b9931b738315d03baeb858826afa523ab628fcc
Artur Fogiel <arturprojekty1988@gmail.com>
2018-10-09 21:07:08 +0200

    Switched aws compilation to mingw

--- deb_aws/build.bat
+++ deb_aws/build.bat
@@ -3,7 +3,7 @@ git clone --depth 1 https://github.com/aws/aws-sdk-cpp.git
 cd aws-sdk-cpp
 
 mkdir build && cd build
-cmake -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=ON -DENABLE_TESTING=OFF -DBUILD_ONLY=s3 -DBUILD_ONLY=transfer ..
+cmake -G "MinGW Makefiles" -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=ON -DENABLE_TESTING=OFF -DBUILD_ONLY=s3 -DBUILD_ONLY=transfer ..
 
 cmake --build . -j 2
 
