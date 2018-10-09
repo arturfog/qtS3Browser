5e0905366af103191c4481fb5efedf6fd41f43f0
Artur Fogiel <arturprojekty1988@gmail.com>
2018-10-09 19:56:07 +0200

    Fix for cmake

--- deb_aws/build.bat
+++ deb_aws/build.bat
@@ -5,6 +5,6 @@ cd aws-sdk-cpp
 mkdir build && cd build
 cmake -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=ON -DENABLE_TESTING=OFF -DBUILD_ONLY=s3 -DBUILD_ONLY=transfer ..
 
-cmake --build -j 2 . 
+cmake --build . -j 2
 
 cd ..\..\
