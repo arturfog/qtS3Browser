cc4d37721a3a44ff01392c1f1ac80b0866a6c9b6
Artur Fogiel <arturprojekty1988@gmail.com>
2018-10-09 19:52:02 +0200

    aws failed to build

--- deb_aws/build.bat
+++ deb_aws/build.bat
@@ -3,8 +3,8 @@ git clone --depth 1 https://github.com/aws/aws-sdk-cpp.git
 cd aws-sdk-cpp
 
 mkdir build && cd build
-cmake -G "MSYS Makefiles" -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=ON -DENABLE_TESTING=OFF -DBUILD_ONLY=s3 -DBUILD_ONLY=transfer ..
+cmake -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=ON -DENABLE_TESTING=OFF -DBUILD_ONLY=s3 -DBUILD_ONLY=transfer ..
 
-mingw32-make -j2
+cmake --build -j 2 . 
 
 cd ..\..\
