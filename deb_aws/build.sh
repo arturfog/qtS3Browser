01acbfcf2ec8064432c41a51ce784829dfa75987
Artur Fogiel <arturprojekty1988@gmail.com>
2018-10-09 19:17:37 +0200

    Switched appveyor build to mingw. Added missing inc and lib paths

--- deb_aws/build.sh
+++ deb_aws/build.sh
@@ -14,6 +14,6 @@ cd aws-sdk-cpp
 mkdir -p build && cd build
 cmake -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=ON -DENABLE_TESTING=OFF -DBUILD_ONLY=s3 -DBUILD_ONLY=transfer ..
 
-make -j 2
+make -j 10
 
 cd ../../
