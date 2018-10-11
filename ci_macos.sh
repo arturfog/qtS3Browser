ec476aa25248c7fbae2f99fb6523530f75c1fc5f
Artur Fogiel <arturprojekty1988@gmail.com>
2018-10-11 18:20:31 +0200

    MacOS testing

--- ci_macos.sh
+++ ci_macos.sh
@@ -1,24 +1,34 @@
 #!/bin/bash
+pwd
+
 brew install qt5 2>&1 > /dev/null
 brew link qt5 --force
 
 # AWS
-cd deb_aws && ./build.sh
+cd deb_aws 
 
+./build.sh 
+
+ls -lh aws-cpp-sdk/build/aws-cpp-sdk-core/
+
+# deb aws
+cd ..
 # QTS3BROWSER
+mkdir -p build
+cd build
 
-mkdir build && cd build
+pwd
 
 # copy AWS
 cp -vf ../deb_aws/aws-cpp-sdk/build/aws-cpp-sdk-core/*.dylib .
-cp -vf ../deb/aws/aws-cpp-sdk/build/aws-cpp-sdk-transfer/*.dylib .
-cp -vf ../deb/aws/aws-cpp-sdk/build/aws-cpp-sdk-s3/*.dylib .
+cp -vf ../deb_aws/aws-cpp-sdk/build/aws-cpp-sdk-transfer/*.dylib .
+cp -vf ../deb_aws/aws-cpp-sdk/build/aws-cpp-sdk-s3/*.dylib .
 
 /usr/local/opt/qt5/bin/qmake ../s3Browser.pro CONFIG+=release && make
 /usr/local/opt/qt5/bin/macdeployqt qts3browser.app -always-overwrite -verbose=2 -qmldir=qml
 
 curl -o /tmp/macdeployqtfix.py https://raw.githubusercontent.com/aurelien-rainone/macdeployqtfix/master/macdeployqtfix.py
 
-python /tmp/macdeployqtfix.py ./qts3browser.app/Contents/MacOS/qts3browser /usr/local/Cellar/qt5/5.11.2_1/
+#python /tmp/macdeployqtfix.py ./qts3browser.app/Contents/MacOS/qts3browser /usr/local/Cellar/qt5/5.11.2_1/
 
-/usr/local/opt/qt5/bin/macdeployqt qts3browser.app -dmg -no-plugins
+#/usr/local/opt/qt5/bin/macdeployqt qts3browser.app -dmg -no-plugins
--- deb_aws/build.sh
+++ deb_aws/build.sh
@@ -11,9 +11,14 @@ git clone --depth 1 https://github.com/aws/aws-sdk-cpp.git
 
 cd aws-sdk-cpp
 
-mkdir -p build && cd build
-cmake -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=ON -DENABLE_TESTING=OFF -DBUILD_ONLY=s3 -DBUILD_ONLY=transfer ..
+mkdir -p build
 
+cd build
+
+cmake -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=ON -DENABLE_TESTING=OFF -DBUILD_ONLY=s3 -DBUILD_ONLY=transfer ..
 make -j 10
 
-cd ../../
+# build
+cd ..
+# aws-sdk-cpp
+cd ..
