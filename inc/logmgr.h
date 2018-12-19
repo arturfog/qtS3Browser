97f3463cbf08e3b56ccbd93ea5cf9937a5b42e49
Artur Fogiel <arturprojekty1988@gmail.com>
2018-12-19 22:18:06 +0100

    Logging to file, first functions added

--- inc/logmgr.h
+++ inc/logmgr.h
@@ -6,18 +6,24 @@
 class LogMgr : public QObject
 {
     Q_OBJECT
-private:
-    static QString logsFolderPath;
 public:
     explicit LogMgr(QObject *parent = nullptr);
-
     static void debug(const std::string msg, const std::string& arg1);
     static void debug(const std::string msg, const QString& arg1);
     static void debug(const std::string msg, const char* arg1);
     static void debug(const std::string msg);
     static void error(const std::string msg);
-
-    static void setLogsFolder(const QString& path);
+    /**
+     * @brief logsEnabled
+     * @return
+     */
+    static bool logsEnabled();
+    /**
+     * @brief logsDir
+     * @return
+     */
+    static QString logsDir();
+    static void writeToFile(const QString &msg);
 };
 
 #endif // LOGMGR_H
--- src/logmgr.cpp
+++ src/logmgr.cpp
@@ -1,40 +1,59 @@
 #include "inc/logmgr.h"
 #include <iostream>
 #include <QDateTime>
+#include <QSettings>
+#include <QFile>
+#include <QTextStream>
 
 LogMgr::LogMgr(QObject *parent) : QObject(parent) {}
-
-QString LogMgr::logsFolderPath("/tmp");
-
+// --------------------------------------------------------------------------
 void LogMgr::debug(const std::string msg, const std::string &arg1)
 {
     const QString now = QDateTime::currentDateTime().toString();
     std::cout << "[DBG]"  << " " << now.toStdString() << " : " << msg  << " ('" << arg1  << "')" << std::endl;
 }
-
+// --------------------------------------------------------------------------
 void LogMgr::debug(const std::string msg, const QString &arg1)
 {
     LogMgr::debug(msg, arg1.toStdString());
 }
-
+// --------------------------------------------------------------------------
 void LogMgr::debug(const std::string msg, const char *arg1)
 {
     LogMgr::debug(msg, std::string(arg1));
 }
-
+// --------------------------------------------------------------------------
 void LogMgr::debug(const std::string msg)
 {
     const QString now = QDateTime::currentDateTime().toString();
     std::cout << "[DBG]"  << " " << now.toStdString() << " : " << msg  << std::endl;
 }
-
+// --------------------------------------------------------------------------
 void LogMgr::error(const std::string msg)
 {
     const QString now = QDateTime::currentDateTime().toString();
     std::cout << "[ERR]"  << " " << now.toStdString() << " : " << msg  << std::endl;
 }
-
-void LogMgr::setLogsFolder(const QString &path)
+// --------------------------------------------------------------------------
+bool LogMgr::logsEnabled()
 {
-    LogMgr::logsFolderPath = path;
+    const QSettings settings;
+    return settings.value("LogsEnabled").toBool();
+}
+// --------------------------------------------------------------------------
+QString LogMgr::logsDir()
+{
+    const QSettings settings;
+    return settings.value("LogsDir").toString();
+}
+// --------------------------------------------------------------------------
+void LogMgr::writeToFile(const QString& msg)
+{
+    if(logsEnabled()) {
+        QFile logfile(logsDir());
+        if(logfile.open(QIODevice::WriteOnly)) {
+            QTextStream stream( &logfile );
+            stream << msg << endl;
+        }
+    }
 }
