88e7650389088ac7c8f606fe321b71822927e882
Artur <arturprojekty1988@gmail.com>
2018-12-22 16:41:44 +0100

    Logging to file is working (#29)

--- inc/logmgr.h
+++ inc/logmgr.h
@@ -2,17 +2,33 @@
 #define LOGMGR_H
 #include <QVariant>
 #include <QObject>
+#include <mutex>
+#include <QFile>
 
 class LogMgr : public QObject
 {
     Q_OBJECT
+private:
+    static QString baseName;
+    static const qint64 LOG_SIZE_LIMIT_MB = 5 * (1024 * 1024);
+    static std::mutex mut;
+    static QFile logfile;
 public:
     explicit LogMgr(QObject *parent = nullptr);
-    static void debug(const std::string msg, const std::string& arg1);
-    static void debug(const std::string msg, const QString& arg1);
-    static void debug(const std::string msg, const char* arg1);
-    static void debug(const std::string msg);
-    static void error(const std::string msg);
+    /**
+     * @brief debug
+     * @param msg
+     * @param arg1
+     */
+    static void debug(const std::string &msg, const std::string& arg1);
+    static void debug(const std::string &msg, const QString& arg1);
+    static void debug(const std::string &msg, const char* arg1);
+    static void debug(const std::string &msg);
+    /**
+     * @brief error
+     * @param msg
+     */
+    static void error(const std::string &msg);
     /**
      * @brief logsEnabled
      * @return
@@ -23,7 +39,20 @@ public:
      * @return
      */
     static QString logsDir();
+    /**
+     * @brief writeToFile
+     * @param msg
+     */
     static void writeToFile(const QString &msg);
+    /**
+     * @brief genLogFilename
+     * @return
+     */
+    static QString genLogFilename(bool rotate = false);
+
+    static int compareModifyTime();
+    static void openLog();
+    static void closeLog();
 };
 
 #endif // LOGMGR_H
--- src/logmgr.cpp
+++ src/logmgr.cpp
@@ -4,32 +4,47 @@
 #include <QSettings>
 #include <QFile>
 #include <QTextStream>
+#include <QFileInfo>
+#include <QDebug>
+
+QString LogMgr::baseName("qts3browser.log");
+std::mutex LogMgr::mut;
+QFile LogMgr::logfile;
 
 LogMgr::LogMgr(QObject *parent) : QObject(parent) {}
 // --------------------------------------------------------------------------
-void LogMgr::debug(const std::string msg, const std::string &arg1)
+void LogMgr::debug(const std::string &msg, const std::string &arg1)
 {
     const QString now = QDateTime::currentDateTime().toString();
-    std::cout << "[DBG]"  << " " << now.toStdString() << " : " << msg  << " ('" << arg1  << "')" << std::endl;
+    QString log("[DBG] ");
+    log.append(now).append(" : ").append(msg.c_str());
+
+    if(!arg1.empty()) {
+        log.append(" (").append(arg1.c_str()).append(") ");
+        std::cout << log.toStdString() << std::endl;
+    } else {
+        std::cout << log.toStdString() << std::endl;
+    }
+
+    writeToFile(log);
 }
 // --------------------------------------------------------------------------
-void LogMgr::debug(const std::string msg, const QString &arg1)
+void LogMgr::debug(const std::string &msg, const QString &arg1)
 {
     LogMgr::debug(msg, arg1.toStdString());
 }
 // --------------------------------------------------------------------------
-void LogMgr::debug(const std::string msg, const char *arg1)
+void LogMgr::debug(const std::string &msg, const char *arg1)
 {
     LogMgr::debug(msg, std::string(arg1));
 }
 // --------------------------------------------------------------------------
-void LogMgr::debug(const std::string msg)
+void LogMgr::debug(const std::string &msg)
 {
-    const QString now = QDateTime::currentDateTime().toString();
-    std::cout << "[DBG]"  << " " << now.toStdString() << " : " << msg  << std::endl;
+    LogMgr::debug(msg, "");
 }
 // --------------------------------------------------------------------------
-void LogMgr::error(const std::string msg)
+void LogMgr::error(const std::string &msg)
 {
     const QString now = QDateTime::currentDateTime().toString();
     std::cout << "[ERR]"  << " " << now.toStdString() << " : " << msg  << std::endl;
@@ -44,16 +59,97 @@ bool LogMgr::logsEnabled()
 QString LogMgr::logsDir()
 {
     const QSettings settings;
-    return settings.value("LogsDir").toString();
+    return settings.value("LogsDir").toString().replace("file://","");
 }
 // --------------------------------------------------------------------------
 void LogMgr::writeToFile(const QString& msg)
 {
     if(logsEnabled()) {
-        QFile logfile(logsDir());
-        if(logfile.open(QIODevice::WriteOnly)) {
-            QTextStream stream( &logfile );
-            stream << msg << endl;
+        std::lock_guard<std::mutex> lock(mut);
+        // log is opened when app is started and logs
+        // are enabled in settings, if logs were enabled
+        // later, log file has to be opened
+        if(!logfile.isOpen()) {
+            openLog();
+        } else if(logfile.size() >= LOG_SIZE_LIMIT_MB) {
+            closeLog();
+            openLog();
+        }
+
+        QTextStream stream( &logfile );
+        stream << msg << endl;
+    }
+}
+// --------------------------------------------------------------------------
+QString LogMgr::genLogFilename(bool rotate)
+{
+    const QString fullPath(logsDir().append("/").append(baseName));
+    for(int i = 0; i < 8; i++)
+    {
+        QString tmpFullPath(fullPath);
+        const QString pathWithNumber(tmpFullPath.append(QString::number(i)));
+        const QFile logfile(pathWithNumber);
+        if(!logfile.exists())
+        {
+            return pathWithNumber;
+        }
+        else if (logfile.size() < LOG_SIZE_LIMIT_MB || rotate)
+        {
+            if(rotate) {
+                const int rotatedNum = compareModifyTime();
+
+                QString tmpFullPath(fullPath);
+                const QString pathWithNumber(tmpFullPath.append(QString::number(rotatedNum)));
+                return pathWithNumber;
+            }
+            return pathWithNumber;
         }
     }
+    // all log files are full. rotate logs.
+    return genLogFilename(true);
+}
+// --------------------------------------------------------------------------
+int LogMgr::compareModifyTime()
+{
+    const QString fullPath(logsDir().append("/").append(baseName));
+    for(int i = 0; i < 8; i++)
+    {
+        QString tmpPath(fullPath);
+        const int current = i % 8;
+        const int next = (i + 1) % 8;
+
+        const QString currPathWithNumber(tmpPath.append(QString::number(current)));
+        const QFileInfo currInfo(currPathWithNumber);
+        const qint64 currTime = currInfo.lastModified().toSecsSinceEpoch();
+
+        tmpPath = fullPath;
+        const QString nextPathWithNumber(tmpPath.append(QString::number(next)));
+        const QFileInfo nextInfo(nextPathWithNumber);
+        const qint64 nextTime = nextInfo.lastModified().toSecsSinceEpoch();
+
+        if(currTime > nextTime) {
+            return next;
+        }
+    }
+
+    return 0;
+}
+// --------------------------------------------------------------------------
+void LogMgr::openLog()
+{
+    if(logsEnabled()) {
+        logfile.setFileName(genLogFilename());
+        if(logfile.size() >= LOG_SIZE_LIMIT_MB) {
+            logfile.remove();
+            logfile.open(QIODevice::WriteOnly | QIODevice::Append);
+        } else {
+            logfile.open(QIODevice::WriteOnly | QIODevice::Append);
+        }
+    }
+}
+// --------------------------------------------------------------------------
+void LogMgr::closeLog()
+{
+    logfile.flush();
+    logfile.close();
 }
